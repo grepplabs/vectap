d59417602144935d2b0eb0a71201836fe3b2fa33
Jan Weinkauff <jan@weinkauff.cloud>
2026-09-14 21:09:43 +0200

    fix(kube): honour multi-path KUBECONFIG

--- internal/kube/config.go
+++ internal/kube/config.go
@@ -1,14 +1,10 @@
 package kube
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 )
 
 func LoadConfig(kubeConfigPath, kubeContext string) (*rest.Config, error) {
@@ -24,26 +20,15 @@ func LoadConfig(kubeConfigPath, kubeContext string) (*rest.Config, error) {
 }
 
 func loadKubeConfig(kubeConfigPath, kubeContext string) (*rest.Config, error) {
-	if kubeConfigPath == "" {
-		kubeConfigPath = os.Getenv("KUBECONFIG")
-	}
-	if kubeConfigPath == "" {
-		home := homedir.HomeDir()
-		if home == "" {
-			return nil, errors.New("home directory not found")
-		}
-		kubeConfigPath = filepath.Join(home, ".kube", "config")
+	loader := clientcmd.NewDefaultClientConfigLoadingRules()
+	if kubeConfigPath != "" {
+		loader.ExplicitPath = kubeConfigPath
 	}
 
-	loader := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfigPath}
 	overrides := &clientcmd.ConfigOverrides{}
 	if kubeContext != "" {
 		overrides.CurrentContext = kubeContext
 	}
-	cfg, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loader, overrides).ClientConfig()
-	if err != nil {
-		return nil, err
-	}
 
-	return cfg, nil
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loader, overrides).ClientConfig()
 }
